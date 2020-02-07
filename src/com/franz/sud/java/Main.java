86faa8607337a7111e12d3b5cd72771475c3ac37
N-141 <n-141@N-257.local>
2020-02-07 13:04:46 +0800

    SUD: Initialize

---
+++ src/com/franz/sud/java/Main.java
@@ -0,0 +1,8 @@
+package com.franz.sud.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/com/franz/sud/java/game/engine/components/Character.java
@@ -0,0 +1,10 @@
+package com.franz.sud.java.game.engine.components;
+
+public class Character {
+    private String name;
+    private Health health;
+
+    public void increaseMaxHealth(int healthValue) {
+
+    }
+}
